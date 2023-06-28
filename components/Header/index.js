529f1ffe0e85658d74ef2efeb83edec1428d5efd
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-06-28 12:20:24 -0300

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,31 +1,55 @@
 // import './styles.js'
-import { StyledHeader} from './styles'
+import React, { useState } from 'react'
+import { StyledHeader, NavLinks, Menu, CloseSidebar, Anchor, Title } from './styles'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+    const [sidebar, setSidebar] = useState(false)
+    const { asPath } = useRouter()
+
+
+    const showSiderbar = () => setSidebar(!sidebar)
+
+    function activeLink(path) {
+        return asPath === `/${path}` ? 'active' : ''
+    }
+
     return (
         <StyledHeader>
             <nav>
-                <ul className='links'>
-                    <li>
-                        {/* <a>Meteora</a> */}
-                        <a><img src='./images/Mobile/Logo mobile.png' /></a>
-                    </li>
-                    <li>
-                        <a href="">
-                            Nossas lojas
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            Novidades
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            Promoções
-                        </a>
-                    </li>
-                </ul>
+                {/* <a>Meteora</a> */}
+                <Title><img src='./images/Mobile/Logo mobile.png' /></Title>
+                <NavLinks sidebar={sidebar ? 1 : 0}>
+                    {/* <ul className='links'> */}
+                    {/* <li> */}
+                    {/* </li> */}
+                    {/* <li> */}
+                    <CloseSidebar onClick={showSiderbar}><img src='./images/marca-cruzada 1.svg' /></CloseSidebar>
+                    <Anchor className={activeLink('') || activeLink('#')} onClick={showSiderbar} href="#">
+                        Home
+                    </Anchor>
+                    <hr />
+                    <Anchor onClick={showSiderbar} href="#">
+                        Nossas lojas
+                    </Anchor>
+                    <hr />
+                    {/* </li> */}
+                    {/* <li> */}
+                    <Anchor onClick={showSiderbar} href="#">
+                        Novidades
+                    </Anchor>
+                    <hr />
+                    {/* </li> */}
+                    {/* <li> */}
+                    <Anchor onClick={showSiderbar} href="#">
+                        Promoções
+                    </Anchor>
+                    {/* </li> */}
+                    {/* </ul> */}
+                </NavLinks>
+                <Menu>
+                    <img src='./images/Icon Color.svg' onClick={showSiderbar} />
+                </Menu>
                 <ul className='search'>
                     <li>
                         <input type="text" placeholder="" />
--- components/Header/styles.js
+++ components/Header/styles.js
@@ -4,20 +4,31 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
 background-color: #000000;
 
-height: 58px;   
+height: 58px;
+display: flex;
+justify-content: space-between;
+padding: 0 20px;   
 nav{
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    padding: 0 20px; 
     ul{
         display: flex;
     }
     ul.links{
        gap: 40px;
     }
+    ul.search{
+      @media (max-width: 768px) {
+      display: none;
+      }
+    }
 }
 ul{
     padding: 0;
-    margin: 0;
+    // margin: 512px;
+    margin: 256px;
     list-style: none;
     color: #FFFFFF;
     a:link { color: inherit;
@@ -27,3 +38,98 @@ ul{
          { color:inherit; }
 }   
  `;
+
+ export const NavLinks = styled.nav`
+  hr{
+    display: none;
+  }
+  display: flex;
+  gap: 20px;
+  @media (max-width: 768px) {
+    z-index: 2;/*evita que a barra de menu fique por baixo da imagem*/
+    flex-direction: column;
+    background-color: #343A40;
+    position: fixed;
+    height: 50%;
+    gap: 0px;
+    top: 0px;
+    right: 0px;
+    width: 40%;
+    right: ${props => props.sidebar ? '0' : '-100%'};
+    transition: 0.5s;
+    overflow-x: auto;                          
+    &::-webkit-scrollbar {
+      width: 12px;
+      border-radius: 10px;
+      background-color: whitesmoke; 
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background-color: darkgray; 
+    } 
+    a {
+      margin-top: 15px; /*assim a barra de menu fica melhor em telas menores*/
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+    }
+    hr{
+      display: block;
+      border: 1px solid #FFFFFF;
+      width: 75%;
+    }
+  }
+`;
+
+export const Menu = styled.div`
+
+  margin-left: 100%;
+  font-size: 30px;
+  display: none;
+  cursor: pointer;
+  
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const CloseSidebar = styled.div`
+  font-size: 30px;
+  right: 15px;
+  top: 15px;
+  position: absolute;
+  display: none;
+  cursor: pointer;
+  
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const Anchor = styled.a`
+  font-size: 16px;
+  height: 100px;
+  transition: 0.3s;
+  color: #FFFFFF;
+  @media (max-width: 768px) {
+  &.active {
+    color: #DAFF01 !important;
+
+  }
+  
+}
+  @media (min-width: 768px) {
+    align-items: center;
+    display: flex;
+    padding: 0px 10px;
+  }
+  &:link { color: #FFFFFF ;
+    text-decoration: none;
+    }
+  &:visited
+     { color: #FFFFFF; }
+`;
+
+export const Title = styled.div`
+  
+`;
