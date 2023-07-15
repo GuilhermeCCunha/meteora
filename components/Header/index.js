689817dffc67227acdd608f242769609e02cfbce
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-07-14 22:01:00 -0300

    header 1.8

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,3 @@
-// import './styles.js'
 import React, { useState } from 'react'
 import { StyledHeader, NavLinks, Menu, CloseSidebar, Anchor, Title } from './styles'
 import { useRouter } from 'next/router'
@@ -11,7 +10,6 @@ export default function Header({ onQuery }) {
     const [sidebar, setSidebar] = useState(false)
     const { asPath } = useRouter()
 
-
     const showSiderbar = () => setSidebar(!sidebar)
 
     function activeLink(path) {
@@ -21,13 +19,8 @@ export default function Header({ onQuery }) {
     return (
         <StyledHeader>
             <nav>
-                {/* <a>Meteora</a> */}
                 <Title><img src='./images/Mobile/Logo mobile.png' /></Title>
                 <NavLinks sidebar={sidebar ? 1 : 0}>
-                    {/* <ul className='links'> */}
-                    {/* <li> */}
-                    {/* </li> */}
-                    {/* <li> */}
                     <CloseSidebar onClick={showSiderbar}><img src='./images/marca-cruzada 1.svg' /></CloseSidebar>
                     <Anchor className={activeLink('') || activeLink('#')} onClick={showSiderbar} href="#">
                         Home
@@ -37,19 +30,13 @@ export default function Header({ onQuery }) {
                         Nossas lojas
                     </Anchor>
                     <hr />
-                    {/* </li> */}
-                    {/* <li> */}
                     <Anchor onClick={showSiderbar} href="#">
                         Novidades
                     </Anchor>
                     <hr />
-                    {/* </li> */}
-                    {/* <li> */}
                     <Anchor onClick={showSiderbar} href="#">
                         Promoções
                     </Anchor>
-                    {/* </li> */}
-                    {/* </ul> */}
                 </NavLinks>
                 <Menu>
                     <img src='./images/Icon Color.svg' onClick={showSiderbar} />
