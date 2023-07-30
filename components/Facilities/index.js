bc0a8be379b0710edf2dd2e582a9ffafcc9ef7f9
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-07-29 21:50:18 -0300

    public

--- components/Facilities/index.js
+++ components/Facilities/index.js
@@ -6,7 +6,7 @@ export default function Facilities() {
             <FacilitiesTitle>Conheça todas as nossas facilidades</FacilitiesTitle>
             <FacilitiesFlexbox>
                 <FacilitiesItem>
-                    <img src="./images/Desktop/Ícones/x-diamond (2).png" alt="Pagamento pelo pix" />
+                    <img src="./images/desktop/icones/x-diamond.png" alt="Pagamento pelo pix" />
                     <FacilitiesItemText>
                         <h1>PAGUE PELO PIX</h1>
                         <p>Ganhe 5% OFF em pagamentos via PIX</p>
@@ -14,14 +14,14 @@ export default function Facilities() {
                 </FacilitiesItem>
 
                 <FacilitiesItem>
-                    <img src="./images/Desktop/Ícones/arrow-repeat (2).png" alt="Troca Grátis" />
+                    <img src="./images/desktop/icones/arrow-repeat.png" alt="Troca Grátis" />
                     <FacilitiesItemText>
                         <h1>TROCA GRÁTIS</h1>
                         <p>Fique livre para trocar em até 30 dias.</p>
                     </FacilitiesItemText>
                 </FacilitiesItem>
                 <FacilitiesItem>
-                    <img src="./images/Desktop/Ícones/flower1 (3).png" alt="SUSTENTABILIDADE" />
+                    <img src="./images/desktop/icones/flower1.png" alt="SUSTENTABILIDADE" />
                     <FacilitiesItemText>
                         <h1>SUSTENTABILIDADE</h1>
                         <p>Moda responsável, que respeita o meio ambiente.</p>
