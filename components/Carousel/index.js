fea8f61b46f8ac49d13ab4b731a44a6b8bbd8995
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-07-31 23:33:22 -0300

    carousel 1.5

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -32,9 +32,33 @@ export default () => {
         className="mySwiper"
       >
         <SwiperNavButtons />
-        <SwiperSlide><StyledImage> <img src='./images/desktop/banner-carousel1_1440.png' className='img1' /> <img src='./images/tablet/banner-carousel1_768.png' className='img2' /> <img src='./images/mobile/banner-carousel1_375.png' className='img3' /> </StyledImage ></SwiperSlide>
-        <SwiperSlide><StyledImage> <img src='./images/desktop/banner-carousel2_1440.png' className='img1' /> <img src='./images/tablet/banner-carousel2_768.png' className='img2' /> <img src='./images/mobile/banner-carousel2_375.png' className='img3' />  </StyledImage ><StyledDiv2><h1>COLEÇÃO ATEMPORAL</h1>Estilo e qualidade para durar.</StyledDiv2></SwiperSlide>
-        <SwiperSlide><StyledImage> <img src='./images/desktop/banner-carousel3_1440.png' className='img1' /> <img src='./images/tablet/banner-carousel3_768.png' className='img2' /> <img src='./images/mobile/banner-carousel3_375.png' className='img3' />  </StyledImage ><StyledDiv><h1>COLEÇÃO ATEMPORAL</h1>Alto impacto visual, baixo impacto ambiental!</StyledDiv></SwiperSlide>
+        <SwiperSlide>
+          <StyledImage>
+            <img src='./images/desktop/banner-carousel1_1440.png' alt="banner" className='img1' />
+            <img src='./images/tablet/banner-carousel1_768.png' alt="banner" className='img2' />
+            <img src='./images/mobile/banner-carousel1_375.png' alt="banner" className='img3' />
+          </StyledImage>
+        </SwiperSlide>
+        <SwiperSlide>
+          <StyledImage>
+            <img src='./images/desktop/banner-carousel2_1440.png' alt="banner" className='img1' />
+            <img src='./images/tablet/banner-carousel2_768.png' alt="banner" className='img2' />
+            <img src='./images/mobile/banner-carousel2_375.png' alt="banner" className='img3' />
+          </StyledImage>
+          <StyledDiv2>
+            <h1>COLEÇÃO ATEMPORAL</h1>Estilo e qualidade para durar.
+          </StyledDiv2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <StyledImage>
+            <img src='./images/desktop/banner-carousel3_1440.png' alt="banner" className='img1' />
+            <img src='./images/tablet/banner-carousel3_768.png' alt="banner" className='img2' />
+            <img src='./images/mobile/banner-carousel3_375.png' alt="banner" className='img3' />
+          </StyledImage>
+          <StyledDiv>
+            <h1>COLEÇÃO ATEMPORAL</h1>Alto impacto visual, baixo impacto ambiental!
+          </StyledDiv>
+        </SwiperSlide>
       </Swiper>
     </Container>
   );
