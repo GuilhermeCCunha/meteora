e1d9a7ac965135f09fb98450100ed478bddd981b
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-08-08 23:52:54 -0300

    categories 1.1

--- components/Categories/index.js
+++ components/Categories/index.js
@@ -10,60 +10,78 @@ export default function Categories({ category, setCategory }) {
 
                 <Item onClick={() => category === "Camisetas" ? setCategory("") : setCategory("Camisetas")}>
                     <Image
-                        src="/images/desktop/Categorias/Categoria camiseta.png"
+                        src="/images/desktop/categorias/categoria-camiseta.png"
                         alt="Categoria camisetas"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Camisetas</Subtitles>
                 </Item>
 
                 <Item onClick={() => category === "Bolsas" ? setCategory("") : setCategory("Bolsas")}>
                     <Image
-                        src="/images/desktop/Categorias/Categoria Bolsa.png"
+                        src="/images/desktop/categorias/categoria-bolsa.png"
                         alt="Categoria bolsas"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Bolsas</Subtitles>
                 </Item>
 
                 <Item onClick={() => category === "Calçados" ? setCategory("") : setCategory("Calçados")}>
                     <Image
-                        src="/images/desktop/Categorias/Categoria Calçados.png"
+                        src="/images/desktop/categorias/categoria-calcados.png"
                         alt="Categoria calçados"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Calçados</Subtitles>
                 </Item>
 
                 <Item onClick={() => category === "Calças" ? setCategory("") : setCategory("Calças")}>
                     <Image
-                        src="/images/desktop/Categorias/Categoria calça.png"
+                        src="/images/desktop/categorias/categoria-calca.png"
                         alt="Categoria calças"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Calças</Subtitles>
                 </Item>
 
                 <Item onClick={() => category === "Casacos" ? setCategory("") : setCategory("Casacos")} >
                     <Image
-                        src="/images/desktop/Categorias/Categoria casacos.png"
+                        src="/images/desktop/categorias/categoria-casacos.png"
                         alt="Categoria casacos"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Casacos</Subtitles>
                 </Item>
 
                 <Item onClick={() => category === "Óculos" ? setCategory("") : setCategory("Óculos")} >
                     <Image
-                        src="/images/desktop/Categorias/Categoria óculos.png"
+                        src="/images/desktop/categorias/categoria-oculos.png"
                         alt="Categoria óculos"
                         width={160}
                         height={157}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
                     />
                     <Subtitles>Óculos</Subtitles>
                 </Item>
