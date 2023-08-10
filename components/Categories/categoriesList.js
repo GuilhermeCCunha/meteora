4ea187c692ebb3864606c9e509d2a89899a0e0fa
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-08-10 01:56:04 -0300

    categories 1.2

---
+++ components/Categories/categoriesList.js
@@ -0,0 +1,40 @@
+export const CATEGORIES = [
+
+    {
+        id: 1,
+        iconUrl: '/images/desktop/categorias/categoria-camiseta.png',
+        name: 'Camisetas',
+        description: 'Categoria camisetas'
+    },
+    {
+        id: 2,
+        iconUrl: '/images/desktop/categorias/categoria-bolsa.png',
+        name: 'Bolsas',
+        description: 'Categoria bolsas'
+    },
+    {
+        id: 3,
+        iconUrl: '/images/desktop/categorias/categoria-calcados.png',
+        name: 'Calçados',
+        description: 'Categoria calçados'
+    },
+    {
+        id: 4,
+        iconUrl: '/images/desktop/categorias/categoria-calca.png',
+        name: 'Calças',
+        description: 'Categoria calças'
+    },
+    {
+        id: 5,
+        iconUrl: '/images/desktop/categorias/categoria-casacos.png',
+        name: 'Casacos',
+        description: 'Categoria casacos'
+    },
+    {
+        id: 6,
+        iconUrl: '/images/desktop/categorias/categoria-oculos.png',
+        name: 'Óculos',
+        description: 'Categoria óculos'
+    },
+
+];
--- components/Categories/index.js
+++ components/Categories/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { CategoriesDiv, StyledCategories, Title, Subtitles, Item } from "./styles"
+import { CATEGORIES } from './categoriesList';
 
 export default function Categories({ category, setCategory }) {
 
@@ -7,84 +8,20 @@ export default function Categories({ category, setCategory }) {
         <StyledCategories>
             <Title>Busque por categoria:</Title>
             <CategoriesDiv>
-
-                <Item onClick={() => category === "Camisetas" ? setCategory("") : setCategory("Camisetas")}>
-                    <Image
-                        src="/images/desktop/categorias/categoria-camiseta.png"
-                        alt="Categoria camisetas"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Camisetas</Subtitles>
-                </Item>
-
-                <Item onClick={() => category === "Bolsas" ? setCategory("") : setCategory("Bolsas")}>
-                    <Image
-                        src="/images/desktop/categorias/categoria-bolsa.png"
-                        alt="Categoria bolsas"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Bolsas</Subtitles>
-                </Item>
-
-                <Item onClick={() => category === "Calçados" ? setCategory("") : setCategory("Calçados")}>
-                    <Image
-                        src="/images/desktop/categorias/categoria-calcados.png"
-                        alt="Categoria calçados"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Calçados</Subtitles>
-                </Item>
-
-                <Item onClick={() => category === "Calças" ? setCategory("") : setCategory("Calças")}>
-                    <Image
-                        src="/images/desktop/categorias/categoria-calca.png"
-                        alt="Categoria calças"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Calças</Subtitles>
-                </Item>
-
-                <Item onClick={() => category === "Casacos" ? setCategory("") : setCategory("Casacos")} >
-                    <Image
-                        src="/images/desktop/categorias/categoria-casacos.png"
-                        alt="Categoria casacos"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Casacos</Subtitles>
-                </Item>
-
-                <Item onClick={() => category === "Óculos" ? setCategory("") : setCategory("Óculos")} >
-                    <Image
-                        src="/images/desktop/categorias/categoria-oculos.png"
-                        alt="Categoria óculos"
-                        width={160}
-                        height={157}
-                        placeholder="blur"
-                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
-                        gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
-                    />
-                    <Subtitles>Óculos</Subtitles>
-                </Item>
+                {CATEGORIES.map(CATEGORY =>
+                    <Item key={CATEGORY.id} onClick={() => category === CATEGORY.name ? setCategory("") : setCategory(CATEGORY.name)}>
+                        <Image
+                            src={CATEGORY.iconUrl}
+                            alt={CATEGORY.description}
+                            width={160}
+                            height={157}
+                            placeholder="blur"
+                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEU
+                            gAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/Z/pPwAHdgLdvIWTjAAAAABJRU5ErkJggg=="
+                        />
+                        <Subtitles>{CATEGORY.name}</Subtitles>
+                    </Item>
+                )}
 
             </CategoriesDiv>
         </StyledCategories>
